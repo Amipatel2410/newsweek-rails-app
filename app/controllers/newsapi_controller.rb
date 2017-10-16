1e5cdf22b08d32e01bb97c110960cae4a283216e
amisha patel <ami.patel2410@gmail.com>
2017-10-16 15:48:18 -0400

    remove services folder

--- app/controllers/newsapi_controller.rb
+++
@@ -1,9 +0,0 @@
-class NewsapiController < ApplicationController
-
-def index
-  newssearch_object = Newssearch.new("top")
-  @article = newssearch_object.get_news()
-  render json: @article
-end
-
-end
